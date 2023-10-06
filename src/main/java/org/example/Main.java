065358dfa56f313c87f584f1c74a73b744cfa14a
Plum Creative <denys.lyulchak333@gmail.com>
2023-10-06 13:43:13 +0200

    Denys + Lyess Tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,8 @@
 package org.example;
 
-import back.Lesson;
-import back.Module;
-import back.Program;
-
 public class Main {
     public static void main(String[] args) {
 
-
+        System.out.println("Hello world!");
     }
 }
