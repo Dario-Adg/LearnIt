62dacc65752b37ab029e6f4e9939f3110ad63a63
salahMesmes <salahmestaoui0@gmail.com>
2023-10-06 17:11:05 +0200

    update commit

--- src/main/java/back/Module.java
+++ src/main/java/back/Module.java
@@ -1,15 +1,112 @@
 package back;
 
+
+import java.sql.*;
 import java.util.List;
 
 public class Module {
     public String Name;
     public String Description;
     public List<ModuleLesson> ModuleLessons;
+    public Connection conn = null;
+    public PreparedStatement stmt = null;
+    public Module(String name, String description, List<ModuleLesson> moduleLessons) {
 
-    public Module (String name, String description, List<ModuleLesson> moduleLessons){
+    }
+
+    public String Module(String name, String description, List<ModuleLesson> moduleLessons) {
         this.Name = name;
         this.Description = description;
         this.ModuleLessons = moduleLessons;
     }
+
+    public void AddModule() {
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+            String sql = "INSERT INTO module (Name, Description,ModuleLessons) VALUES(?, ?)";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, Name);
+            stmt.setString(2, Description);
+            stmt.setArray(2, (Array) ModuleLessons);
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+        
+
+
+    public void DeleteModule() {
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+            String sql = "DELETE FROM module WHERE id=?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, Name);
+            stmt.setString(2, Description);
+            stmt.setArray(2, (Array) ModuleLessons);
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+
+    public void ModifyModule(){
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+            String sql = "";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, Name);
+            stmt.setString(2, Description);
+            stmt.setArray(2, (Array) ModuleLessons);
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+
+    public class ModuleLessonDeleter(Array){
+
+        public boolean deleteModuleLesson(int moduleLessonId){
+
+            try{
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+                String sql = "DELETE FROM module_lessons WHERE id = ?";
+
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,26 @@
 package org.example;
 
+import back.Lesson;
+import back.Module;
+import back.ModuleLesson;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+
         System.out.println("Hello world!");
+        Lesson lesson = new Lesson("test", "test");
+
+        Module module = new Module("name", "description", null);
+            
+        CreateModule ("name", "description", "null");
+        
+    }
+
+    private static void CreateModule(String name, String description, String aNull) {
     }
 }
