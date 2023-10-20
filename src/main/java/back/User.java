35856bb326bb2eb7ed147426a5aca0a9871460bb
BeaMoura0906 <bea-moura@outlook.fr>
2023-10-20 15:20:45 +0200

    Méthodes UpdateUserAsAdmin + UpdateUserAsUser + DeleteUser / + AllUsersAsAdmin

--- src/main/java/back/User.java
+++ src/main/java/back/User.java
@@ -4,10 +4,13 @@ import java.sql.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class User {
+    public int Id;
     public String FirstName;
     public String LastName;
     public String Email;
@@ -30,6 +33,74 @@ public class User {
         this.DiplomaNumber = DiplomaNumber;
         this.UserPrograms = userPrograms;
         this.DateOfBirth = dateOfBirth;
+    }
+
+    public static ArrayList<HashMap> allUsers(){
+
+        Connection Conn = ConnectionBDD.ConnectionBDD();
+
+        Statement Stmt = null;
+
+        ResultSet Rs = null;
+
+        ArrayList<HashMap> users = new ArrayList<>();
+
+        /*
+        DateFormat Df = new SimpleDateFormat("yyyy-MM-dd");
+        Date Date = null;
+
+        try {
+            Date = Df.parse(dateOfBirth);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        */
+
+        try {
+            Conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+            String SqlQuery = "SELECT * FROM user";
+            Stmt = Conn.createStatement();
+            Rs = Stmt.executeQuery(SqlQuery);
+
+            //Nom des Index dans la BDD : `Email`, `Password`, `FirstName`, `LastName`, `IsAdmin`, `IsJobSeeker`, `DateOfBirth`, `DiplomaNumber`
+            while(Rs.next()){
+                Integer id = Rs.getInt("Id");
+                String email = Rs.getString("Email");
+                String password = Rs.getString("Password");
+                String firstName = Rs.getString("FirstName");
+                String lastName = Rs.getString("LastName");
+                boolean isAdmin = Rs.getBoolean("IsAdmin");
+                boolean isJobSeeker = Rs.getBoolean("IsJobSeeker");
+                java.sql.Date dateOfBirth = Rs.getDate("DateOfBirth");
+
+                //Convertir java.sql.Date en java.util.Date
+
+                Integer diplomaNumber = Rs.getInt("DiplomaNumber");
+
+                HashMap user = new HashMap();
+                user.put("Id", id);
+                user.put("Email", email);
+                user.put("Password", password);
+                user.put("FirstName", firstName);
+                user.put("LastName", lastName);
+                user.put("IsAdmin", isAdmin);
+                user.put("IsJobSeeker", isJobSeeker);
+                user.put("DateOfBirth", dateOfBirth);
+
+
+                users.add(user);
+
+            }
+
+        } catch (SQLException ex) {
+            //Handle any errors
+            System.out.println("SQLException : " +ex.getMessage());
+            System.out.println("SQLState : " + ex.getSQLState());
+            System.out.println("VendorError : " + ex.getErrorCode());
+        }
+
+        return users;
+
     }
 
     public static void AddUser(String email, String password, String firstName, String lastName, boolean isJobSeeker,
@@ -76,4 +147,125 @@ public class User {
         }
 
     }
+
+    public static void UpdateUserAsAdmin(String email, String password, String firstName, String lastName, boolean isAdmin,
+                                         boolean isJobSeeker, String dateOfBirth, Integer diplomaNumber, Integer id){
+
+        Connection Conn = ConnectionBDD.ConnectionBDD();
+
+        PreparedStatement Stmt = null;
+
+        DateFormat Df = new SimpleDateFormat("yyyy-MM-dd");
+        Date Date = null;
+
+        try {
+            Date = Df.parse(dateOfBirth);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+            String SqlQuery = "UPDATE user SET `Email` = ?, `Password` = ?, `FirstName` = ?, `LastName` = ?, `IsAdmin` = ?, `IsJobSeeker` = ?, `DateOfBirth` = ?, `DiplomaNumber` = ? WHERE `id` = ?";
+
+            Stmt = Conn.prepareStatement(SqlQuery);
+            Stmt.setString(1, email);
+            Stmt.setString(2, password);
+            Stmt.setString(3, firstName);
+            Stmt.setString(4, lastName);
+            Stmt.setBoolean(5, isAdmin);
+            Stmt.setBoolean(6, isJobSeeker);
+            Stmt.setDate(7, new java.sql.Date(Date.getTime()));
+            Stmt.setInt(8, diplomaNumber);
+            Stmt.setInt(9, id);
+
+            Stmt.executeUpdate();
+
+            // Fermeture du Statement
+            if(Stmt != null) {
+                Stmt.close();
+            }
+
+        } catch (SQLException ex) {
+            //Handle any errors
+            System.out.println("SQLException : " +ex.getMessage());
+            System.out.println("SQLState : " + ex.getSQLState());
+            System.out.println("VendorError : " + ex.getErrorCode());
+        }
+
+    }
+
+    public static void UpdateUserAsUser(String email, String password, String firstName, String lastName, String dateOfBirth, Integer diplomaNumber, Integer id){
+
+        Connection Conn = ConnectionBDD.ConnectionBDD();
+
+        PreparedStatement Stmt = null;
+
+        DateFormat Df = new SimpleDateFormat("yyyy-MM-dd");
+        Date Date = null;
+
+        try {
+            Date = Df.parse(dateOfBirth);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+            String SqlQuery = "UPDATE user SET `Email` = ?, `Password` = ?, `FirstName` = ?, `LastName` = ?, `IsAdmin` = ?, `IsJobSeeker` = ?, `DateOfBirth` = ?, `DiplomaNumber` = ? WHERE `id` = ?";
+
+            Stmt = Conn.prepareStatement(SqlQuery);
+            Stmt.setString(1, email);
+            Stmt.setString(2, password);
+            Stmt.setString(3, firstName);
+            Stmt.setString(4, lastName);
+            Stmt.setDate(5, new java.sql.Date(Date.getTime()));
+            Stmt.setInt(6, diplomaNumber);
+            Stmt.setInt(7, id);
+
+            Stmt.executeUpdate();
+
+            // Fermeture du Statement
+            if(Stmt != null) {
+                Stmt.close();
+            }
+
+        } catch (SQLException ex) {
+            //Handle any errors
+            System.out.println("SQLException : " +ex.getMessage());
+            System.out.println("SQLState : " + ex.getSQLState());
+            System.out.println("VendorError : " + ex.getErrorCode());
+        }
+
+    }
+
+    public static void DeleteUser(Integer id){
+
+        Connection Conn = ConnectionBDD.ConnectionBDD();
+
+        PreparedStatement Stmt = null;
+
+        try {
+            Conn = DriverManager.getConnection("jdbc:mysql://localhost/learnit", "root", "");
+            String SqlQuery = "DELETE FROM user WHERE Id=?";
+
+            Stmt = Conn.prepareStatement(SqlQuery);
+            Stmt.setInt(1, id);
+
+            Stmt.executeUpdate();
+
+            // Fermeture du Statement
+            if(Stmt != null) {
+                Stmt.close();
+            }
+
+        } catch (SQLException ex) {
+            //Handle any errors
+            System.out.println("SQLException : " +ex.getMessage());
+            System.out.println("SQLState : " + ex.getSQLState());
+            System.out.println("VendorError : " + ex.getErrorCode());
+        }
+
+    }
+
 }
